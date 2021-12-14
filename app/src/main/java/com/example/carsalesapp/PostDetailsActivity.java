341a65edbf4d457db6896aa3a3537f4d5b7ab616
mohkhe <mohanad.kh33@gmail.com>
2021-12-14 06:33:37 +0200

    Fix share with other apps

--- app/src/main/java/com/example/carsalesapp/PostDetailsActivity.java
+++ app/src/main/java/com/example/carsalesapp/PostDetailsActivity.java
@@ -1,11 +1,18 @@
 package com.example.carsalesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.example.carsalesapp.adapter.RecyclerViewAdapter;
+import com.example.carsalesapp.converters.Converters;
+import com.example.carsalesapp.viewmodel.CarViewModel;
+
+import util.CarApi;
 
 public class PostDetailsActivity extends AppCompatActivity {
 
@@ -15,6 +22,8 @@ public class PostDetailsActivity extends AppCompatActivity {
     private TextView priceInfo;
     private ImageView CarImageInfo;
     private TextView userNameInfo;
+    private CarViewModel carViewModel;
+    private int carId = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,5 +35,23 @@ public class PostDetailsActivity extends AppCompatActivity {
         manufactureInfo = findViewById(R.id.manufactureInfoId);
         priceInfo = findViewById(R.id.priceInfoId);
         CarImageInfo = findViewById(R.id.CarImageInfoId);
+
+        carViewModel = new ViewModelProvider
+                .AndroidViewModelFactory(PostDetailsActivity.this.getApplication())
+                .create(CarViewModel.class);
+
+        if(getIntent().hasExtra(RecyclerViewAdapter.CAR_ID)){
+            carId = getIntent().getIntExtra(MainActivity.CAR_ID,0);
+            carViewModel.get(carId).observe(this, carEntity -> {
+                if (carEntity!=null){
+                    userNameInfo.setText(CarApi.getInstance().getUserName());
+                    modelInfo.setText(carEntity.getModel());
+                    descriptionInfo.setText(carEntity.getDescription());
+                    manufactureInfo.setText(carEntity.getManufacturer());
+                    priceInfo.setText(carEntity.getPrice().toString());
+                    CarImageInfo.setImageBitmap(Converters.ByteToBitMap(carEntity.getImage()));
+                }
+            });
+        }
     }
 }
--- app/src/main/java/com/example/carsalesapp/adapter/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/carsalesapp/adapter/RecyclerViewAdapter.java
@@ -1,11 +1,13 @@
 package com.example.carsalesapp.adapter;
 
+import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Environment;
+import android.os.StrictMode;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,15 +20,18 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.core.content.FileProvider;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.carsalesapp.AddCarActivity;
+import com.example.carsalesapp.CurrentUserActivity;
 import com.example.carsalesapp.PostDetailsActivity;
 import com.example.carsalesapp.R;
 import com.example.carsalesapp.converters.Converters;
 import com.example.carsalesapp.model.CarEntity;
 import com.example.carsalesapp.model.CarInformation;
 import com.example.carsalesapp.model.UserInformation;
+import com.example.carsalesapp.viewmodel.CarViewModel;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
 
@@ -36,10 +41,13 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
 
+import util.CarApi;
+
 public class RecyclerViewAdapter extends RecyclerView.Adapter <RecyclerViewAdapter.ViewHolder>{
     private List<CarEntity> carList;
     private Context context;
-
+    private CarViewModel carViewModel;
+    public static final String CAR_ID = "car_id";
     public RecyclerViewAdapter(List<CarEntity> carList, Context context) {
         this.carList = carList;
         this.context = context;
@@ -68,7 +76,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter <RecyclerViewAdapt
         return Objects.requireNonNull(carList.size());
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder {
         public TextView Model;
         public TextView Manufacturer;
         public TextView Price;
@@ -80,12 +88,12 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter <RecyclerViewAdapt
         public ViewHolder(@NonNull View itemView, Context ctx) {
             super(itemView);
             context = ctx;
-            Model = itemView.findViewById(R.id.modelInfoId);
-            Manufacturer = itemView.findViewById(R.id.manufactureInfoId);
-            Price = itemView.findViewById(R.id.priceInfoId);
-            Owner = itemView.findViewById(R.id.userNameInfoId);
-            Description = itemView.findViewById(R.id.descriptionInfoId);
-            carImage = itemView.findViewById(R.id.CarImageInfoId);
+            Model = itemView.findViewById(R.id.model);
+            Manufacturer = itemView.findViewById(R.id.manufacturer);
+            Price = itemView.findViewById(R.id.price);
+            Owner = itemView.findViewById(R.id.userName);
+            //Description = itemView.findViewById(R.id.descriptionInfoId);
+            carImage = itemView.findViewById(R.id.recyclerCarImage);
             shareButton = itemView.findViewById(R.id.shareBtnId);
             seeMoreButton = itemView.findViewById(R.id.seeMoreBtnId);
             shareButton.setOnClickListener(new View.OnClickListener() {
@@ -99,8 +107,28 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter <RecyclerViewAdapt
 //                    Intent shareIntent = Intent.createChooser(sendIntent, null);
 //                    context.startActivity(shareIntent);
                     int position = getAdapterPosition();
-                    String imageUrl = carList.get(position).getImage().toString();
-                    shareImage(imageUrl, ctx, position, carList);
+                    carViewModel = new ViewModelProvider
+                            .AndroidViewModelFactory((Application) context.getApplicationContext())
+                            .create(CarViewModel.class);
+
+                    //String imageUrl = carList.get(position).getImage().toString();
+                    //shareImage(imageUrl, ctx, position, carList);
+                    StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+                    StrictMode.setVmPolicy(builder.build());
+                    if (isExternalStorageWritable()){
+                        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                        shareIntent.putExtra(Intent.EXTRA_SUBJECT, "From Car Sales App");
+                        shareIntent.putExtra(Intent.EXTRA_TEXT, CarApi.getInstance().getUserName()
+                                +", shared with you car offer," +" posted by:"+carList.get(position).getFK()
+                                +"\n\n"+"Car manufacture: " +
+                                 carList.get(position).getManufacturer() + ", car price: " +
+                                 carList.get(position).getPrice());
+
+                        shareIntent.setType("image/*");
+                        shareIntent.putExtra(Intent.EXTRA_STREAM, saveImageExternal(Converters.ByteToBitMap(carList.get(position).getImage())));
+                        context.startActivity(Intent.createChooser(shareIntent, "Send Image"));
+                    }
+
                 }
             });
 
@@ -108,55 +136,71 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter <RecyclerViewAdapt
                 @Override
                 public void onClick(View view) {
                     int position = getAdapterPosition();
-                    Model.setText(carList.get(position).getModel());
-                    Manufacturer.setText(carList.get(position).getManufacturer());
-                    Price.setText(carList.get(position).getPrice().toString());
-                    Owner.setText(carList.get(position).getFK());
-                    Description.setText(carList.get(position).getDescription());
-                    carImage.setImageBitmap(Converters.ByteToBitMap(carList.get(position).getImage()));
                     Intent informationIntent = new Intent(context, PostDetailsActivity.class);
+                    informationIntent.putExtra(CAR_ID,carList.get(position).getId());
                     context.startActivity(informationIntent);
                 }
             });
         }
     }
-    static public void shareImage(String url, final Context context, final int position, final List<CarEntity>carList) {
-
-        Picasso.get().load(url).into(new Target() {
-            @Override public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
-                Intent shareIntent = new Intent(Intent.ACTION_SEND);
-                shareIntent.putExtra(Intent.EXTRA_SUBJECT, "From Car Sales App");
-                shareIntent.putExtra(Intent.EXTRA_TEXT, carList.get(position).getManufacturer()
-                        + "\n\n" + carList.get(position).getPrice());
-
-                shareIntent.setType("image/*");
-
-                Uri imageUri = getLocalBitmapUri(bitmap, context);
-
-                shareIntent.putExtra(Intent.EXTRA_STREAM, imageUri);
-                Log.d("TAG", "onBitmapLoaded: "+imageUri.toString());
-                context.startActivity(Intent.createChooser(shareIntent, "Send Image"));
-            }
-
-            @Override
-            public void onBitmapFailed(Exception e, Drawable errorDrawable) {
-
-            }
-            @Override public void onPrepareLoad(Drawable placeHolderDrawable) { }
-        });
-    }
-    static public Uri getLocalBitmapUri(Bitmap bmp, final Context context) {
-        Uri bmpUri = null;
-        try {
-            File file =  new File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), "share_image_" + System.currentTimeMillis() + ".png");
-            FileOutputStream out = new FileOutputStream(file);
-            bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
-            out.close();
-            bmpUri = FileProvider.getUriForFile(context.getApplicationContext(),"com.example.carsalesapp.fileprovider", file);
-        } catch (IOException e) {
-            e.printStackTrace();
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
         }
-        return bmpUri;
+        return false;
     }
+    private Uri saveImageExternal(Bitmap image) {
+        //TODO - Should be processed in another thread
+        Uri uri = null;
+        try {
+            File file = new File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), "to-share.png");
+            FileOutputStream stream = new FileOutputStream(file);
+            image.compress(Bitmap.CompressFormat.PNG, 90, stream);
+            stream.close();
+            uri = Uri.fromFile(file);
+        } catch (IOException e) {
+            Log.d("TAG", "IOException while trying to write file for sharing: " + e.getMessage());
+        }
+        return uri;
+    }
+//    static public void shareImage(String url, final Context context, final int position, final List<CarEntity>carList) {
+//
+//        Picasso.get().load(url).into(new Target() {
+//            @Override public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+//                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+//                shareIntent.putExtra(Intent.EXTRA_SUBJECT, "From Car Sales App");
+//                shareIntent.putExtra(Intent.EXTRA_TEXT, carList.get(position).getManufacturer()
+//                        + "\n\n" + carList.get(position).getPrice());
+//
+//                shareIntent.setType("image/*");
+//
+//                Uri imageUri = getLocalBitmapUri(bitmap, context);
+//
+//                shareIntent.putExtra(Intent.EXTRA_STREAM, imageUri);
+//                Log.d("TAG", "onBitmapLoaded: "+imageUri.toString());
+//                context.startActivity(Intent.createChooser(shareIntent, "Send Image"));
+//            }
+//
+//            @Override
+//            public void onBitmapFailed(Exception e, Drawable errorDrawable) {
+//
+//            }
+//            @Override public void onPrepareLoad(Drawable placeHolderDrawable) { }
+//        });
+//    }
+//    static public Uri getLocalBitmapUri(Bitmap bmp, final Context context) {
+//        Uri bmpUri = null;
+//        try {
+//            File file =  new File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), "share_image_" + System.currentTimeMillis() + ".png");
+//            FileOutputStream out = new FileOutputStream(file);
+//            bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+//            out.close();
+//            bmpUri = FileProvider.getUriForFile(context.getApplicationContext(),"com.example.carsalesapp.fileprovider", file);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        return bmpUri;
+//    }
 }
 
